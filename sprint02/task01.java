5215e4b85bbe0d896c933cd26e68199b0b90cde1
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-03 15:44:06 +0300

    Update task01.java

--- sprint02/task01.java
+++ sprint02/task01.java
@@ -1,1 +1,26 @@
+/* Link: 
+ * https://leetcode.com/problems/palindrome-number/
+ * 
+ * Task:
+ * Determine whether an integer is a palindrome. 
+ * An integer is a palindrome when it reads the same backward as forward.
+ */
 
+/**
+ * @author Bogdan Kurchak
+ */
+class Solution {
+    public boolean isPalindrome(int x) {
+        int copy = x;
+        int digit = 0;
+        int result = 0;
+
+        while (copy > 0) {
+            digit = copy % 10;
+            result = result * 10 + digit;
+            copy /= 10;
+        }
+
+        return x == result;
+    }
+}
