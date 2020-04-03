658c8cf7b98a20f4263a9ea5c55403961b26b484
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-03 20:25:27 +0300

    Update task04.java

--- sprint02/task04.java
+++ sprint02/task04.java
@@ -1,1 +1,28 @@
+/* Link: 
+ * https://leetcode.com/problems/sqrtx/
+ * 
+ * Task:
+ * Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+ * Since the return type is an integer, the decimal digits are truncated 
+ * and only the integer part of the result is returned.
+ */
 
+/**
+ * @author Bogdan Kurchak
+ */
+class Solution {
+    public int mySqrt(int x) {
+        long product = 0;
+        int i;
+
+        for (i = 0; i <= x; i++) {
+            product = (long)i * i;
+
+            if (product >= x) {
+                break;
+            }
+        }
+
+        return (product > x) ? (i - 1) : i;
+    }
+}
