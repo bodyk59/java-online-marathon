84e484405ff32e7d072b3f72b0582e2ea78b3702
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-07 19:02:10 +0300

    Added description to task01.java

--- sprint03/task01.java
+++ sprint03/task01.java
@@ -1,6 +1,13 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
+/*
+ * Create a arrSort() method of the MyUtils class to sort a two-dimensional integer array 
+ * by descending the elements of the first column.  
+ * In the case of equality of elements in the first column, 
+ * the elements of the second column must be sorted in ascending order.
+ */
+
 /**
  * @author Bogdan Kurchak
  */
