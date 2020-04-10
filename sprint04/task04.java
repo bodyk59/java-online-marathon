2fa656ecb7addfc65783ccf8ea06b334fc0367e2
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-10 11:34:33 +0300

    Create task04.java

---
+++ sprint04/task04.java
@@ -0,0 +1,33 @@
+/*
+ * Create a new version of the Circle class where the draw method will be overloaded three times: 
+ * 1. Using the one parameter color of String type.
+ * 2. Using the one parameter scale of float type.
+ * 3. Using two parameters color and scale of String and float type.
+ */
+ 
+/**
+ * @author Bogdan Kurchak
+ */
+class Circle {
+  private double radius;
+  
+  public Circle(double radius) {
+    this.radius = radius;
+  }
+  
+  public void draw() {
+  
+  }
+
+  public void draw(String color) {
+        
+  }
+    
+  public void draw(float scale) {
+      
+  }
+    
+  public void draw(String color, float scale) {
+        
+  }
+}
