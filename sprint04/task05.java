9466833cca3696bb122823cbebd80f5587c69390
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-10 11:55:04 +0300

    Updated description to task05.java

--- sprint04/task05.java
+++ sprint04/task05.java
@@ -1,8 +1,11 @@
 /*
- * Create a new version of the Circle class where the draw method will be overloaded three times: 
- * 1. Using the one parameter color of String type.
- * 2. Using the one parameter scale of float type.
- * 3. Using two parameters color and scale of String and float type.
+ * A Point class, which models a 2D point with x and y coordinates should contains:
+ * 1. Two private instance variables x and y  of int type.
+ * 2. A constructor that constructs a point with the given x and y coordinates.
+ * 3. A method getXYPair() which returns the x and y in a 2-element int array.
+ * 4. A method called distance(int x, int y) that returns the distance from this point to another point at the given (x, y) coordinates.
+ * 5. An overloaded distance(Point point) method that returns the distance from this point to the given Point instance.
+ * 6. Another overloaded distance() method that returns the distance from this point to the origin (0, 0).
  */
  
 /**
