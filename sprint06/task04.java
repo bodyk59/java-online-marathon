fc5620b3a8f456075fa51d0bc31e7a4efb14900a
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-16 19:14:26 +0300

    Create task04.java

---
+++ sprint06/task04.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/*
+ * Please create class Shape with abstract method to calculate area of figure and field name.
+ * Replace code in method getArea() according to principles of polymorphism i.e.
+ * Circle and Rectangle classes extends Shape class and override double getArea() method.
+ * Develop maxAreas() method of the MyUtils class to return a
+ * Map with class name as key and instance list with maximum area as value.
+ * The original list must be unchanged.
+ * For example, for a given list
+ * [Circle [radius=2.00],
+ * Rectangle [height=2.00, width=3.00],
+ * Circle [radius=1.00], Rectangle [height=3.00, width=2.00],
+ * Circle [radius=0.50], Rectangle [height=1.00, width=2.00]]
+ * you should get
+ * {"Circle"=[Circle [radius=2.00]],
+ * "Rectangle"=[Rectangle [height=2.00, width=3.00],
+ * Rectangle [height=3.00, width=2.00]]}
+ */
+
+/**
+ * @author Bogdan Kurchak
+ */
