fbbb6f71ad03f1008678088fcd60c85427a16bff
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-27 16:24:07 +0300

    Create task02.java

---
+++ sprint09/task02.java
@@ -0,0 +1,39 @@
+import java.util.ArrayDeque;
+
+/*
+ * Given a text containing brackets  '(', ')', '{', '}', '[' and ']'.
+ * Sequences "\\(", "\\)", "\\[", "\\]", "\\{" and "\\}" are not brackets.
+ * Write a boolean verifyBrackets(String text) method of the MyUtils class to check a input text.
+ * The brackets must close in the correct order, for example "()", "()[]{}", "{(())}"
+ * and "(\\()" are all valid but "(]", ")(" and "([)]" are not.
+ */
+
+/**
+ * @author Bogdan Kurchak
+ */
+public class MyUtils {
+    public boolean verifyBrackets(String text) {
+        text = text.replaceAll("\\\\([{}()\\[\\]])", "");
+        ArrayDeque<Character> stack  = new ArrayDeque<>();
+
+        for(int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if(c == '[' || c == '(' || c == '{' ) {
+                stack.push(c);
+            } else if(c == ']') {
+                if(stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if(c == ')') {
+                if(stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if(c == '}') {
+                if(stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
