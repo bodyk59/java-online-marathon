37f03ef450f4bf6ee505b8fbf4359b6e61e54d85
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-04 13:09:48 +0300

    Update task02.java

--- sprint10/task02.java
+++ sprint10/task02.java
@@ -9,6 +9,6 @@
 class ArrayUtil {
     public static <T> T setAndReturn(T[] array, T element, int index) {
         array[index] = element;
-        return array[index] = element;
+        return array[index];
     }
 }
