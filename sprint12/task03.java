a0de79c31ca95a2991c7fca5247ac201d99d02c5
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-11 13:20:17 +0300

    Update task03.java

--- sprint12/task03.java
+++ sprint12/task03.java
@@ -27,26 +27,22 @@ import java.util.function.UnaryOperator;
 class Interactor {
     int x;
 
-    public void serve(UnaryOperator<Integer> uo, int initializer) throws InterruptedException{
-       synchronized (this) {
-           this.x = uo.apply(initializer);
-           System.out.println("Serving thread running");
-           System.out.println("Serving thread initializes the key");
-           System.out.println("key = " + this.x);
-           notify();
-           wait();
-       }
+    public synchronized void serve(UnaryOperator<Integer> uo, int initializer) throws InterruptedException{
+       this.x = uo.apply(initializer);
+       System.out.println("Serving thread running");
+       System.out.println("Serving thread initializes the key");
+       System.out.println("key = " + this.x);
+       notify();
+       wait();
        System.out.println("Serving thread resumed");
     }
 
-    public void consume(BinaryOperator<Integer> bo, int operand1) throws InterruptedException{
-        synchronized (this) {
-            wait(3000);
-            System.out.println("Consuming thread received the key. key = " + this.x);
-            this.x = bo.apply(this.x, operand1);
-            System.out.println("Consuming thread changed the key. key = " + this.x);
-            notify();
-        }
+    public synchronized void consume(BinaryOperator<Integer> bo, int operand1) throws InterruptedException{
+        wait(3000);
+        System.out.println("Consuming thread received the key. key = " + this.x);
+        this.x = bo.apply(this.x, operand1);
+        System.out.println("Consuming thread changed the key. key = " + this.x);
+        notify();
     }
 }
 
